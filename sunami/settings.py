2806582244f52e3d7cb32da23b25312bedd6304a
Wojtechnology <wswiders@uwaterloo.ca>
2015-03-11 14:44:18 -0400

    Restructure

--- sunami/settings.py
+++ sunami/settings.py
@@ -58,9 +58,13 @@ WSGI_APPLICATION = 'sunami.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thedream',
+        'USER': 'dolphinwithwings',
+        'PASSWORD': 'musicislife',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    } 
 }
 
 # Internationalization
