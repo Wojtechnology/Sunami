53ffeed895b6fead4e81dc555ca85a910cbfe741
Wojtechnology <wswiders@uwaterloo.ca>
2015-03-11 22:57:40 -0400

    Genesis

--- sunami/settings.py
+++ sunami/settings.py
@@ -26,6 +26,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Application definition
 
--- sunami/urls.py
+++ sunami/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from sunami import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sunami.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name = "index"),
+ 
 )
