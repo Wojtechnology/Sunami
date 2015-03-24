f9bbf4a595e73c329f006115198d42129cfef8a7
Wojtechnology <wswiders@uwaterloo.ca>
2015-03-23 20:48:52 -0400

    urls change

--- sunami/urls.py
+++ sunami/urls.py
@@ -5,9 +5,6 @@ from django.contrib import admin
 from sunami.views import IndexView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sunami.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^profile/', include('accounts.urls', namespace = 'accounts')),
