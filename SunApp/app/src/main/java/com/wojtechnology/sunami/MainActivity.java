1a3f9e5537c352efcf7dfa918dbd4752ecb31a71
Wojtechnology <wswiders@uwaterloo.ca>
2015-04-07 13:25:19 -0400

    Added Toolbar

--- SunApp/app/src/main/java/com/wojtechnology/sunami/MainActivity.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/MainActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
