2fcd72b2c8e9ddccc7ad378a522a94b035c379ad
Wojtechnology <wswiders@uwaterloo.ca>
2015-04-09 13:18:49 -0400

    Navigation Drawer More

--- SunApp/app/src/main/java/com/wojtechnology/sunami/NavigationDrawerFragment.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/NavigationDrawerFragment.java
@@ -4,6 +4,7 @@ package com.wojtechnology.sunami;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,8 @@ import android.support.v7.widget.Toolbar;
  */
 public class NavigationDrawerFragment extends Fragment {
 
-
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
     public NavigationDrawerFragment() {
         // Required empty public constructor
     }
@@ -30,6 +32,22 @@ public class NavigationDrawerFragment extends Fragment {
 
 
     public void setUp(DrawerLayout drawerLayout, Toolbar toolbar) {
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
 
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 }
