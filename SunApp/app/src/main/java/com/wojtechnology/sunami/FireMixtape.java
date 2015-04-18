5b2797c69fd724ce556180203c33feb2c5102c9e
Wojtechnology <wswiders@uwaterloo.ca>
2015-04-18 16:30:16 -0400

    Added More Values for Music

--- SunApp/app/src/main/java/com/wojtechnology/sunami/FireMixtape.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/FireMixtape.java
@@ -16,32 +16,38 @@ import java.io.InputStream;
  * Created by wojtekswiderski on 15-04-13.
  */
 public class FireMixtape {
-    public int icon_id;
 
-    public String _id;
-    public String artist;
-    public String title;
-    public String data;
-    public String display_name;
-    public String duration;
-    public String album_id;
+    // If no artwork available
+    public int default_icon_id;
+
+    // Some required variables for Android functions
+    private Context context;
+
+    // Helpful booollllssss
     public Boolean icon_loaded;
 
-    private static final int ICON_WIDTH = 48;
-    private static final int ICON_HEIGHT = 48;
-    private static float display_density;
-
-    private Context context;
-    private Bitmap art_bitmap;
+    // Song data from SQL
+    public String _id;
+    public String album;
+    public String album_id;
+    public String artist;
+    public String artist_id;
+    public String bookmark;
+    public String composer;
+    public String duration;
+    public String title_key;
+    public String year;
+    public String data;
+    public String date_added;
+    public String display_name;
+    public String mime_type;
+    public String size;
+    public String title;
 
     public FireMixtape(Context context) {
-        this.icon_id = R.mipmap.ic_launcher;
+        this.default_icon_id = R.mipmap.ic_launcher;
         this.context = context;
         this.icon_loaded = false;
-        DisplayMetrics metrics = new DisplayMetrics();
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        wm.getDefaultDisplay().getMetrics(metrics);
-        display_density = metrics.density;
     }
 
     /* public Bitmap getAlbumArt() {
--- SunApp/app/src/main/java/com/wojtechnology/sunami/MainActivity.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/MainActivity.java
@@ -54,12 +54,21 @@ public class MainActivity extends ActionBarActivity {
 
         String[] projection = {
                 MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.ALBUM_ID,
                 MediaStore.Audio.Media.ARTIST,
-                MediaStore.Audio.Media.TITLE,
-                MediaStore.Audio.Media.DATA,
-                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.ARTIST_ID,
+                MediaStore.Audio.Media.BOOKMARK,
+                MediaStore.Audio.Media.COMPOSER,
                 MediaStore.Audio.Media.DURATION,
-                MediaStore.Audio.Media.ALBUM_ID
+                MediaStore.Audio.Media.TITLE_KEY,
+                MediaStore.Audio.Media.YEAR,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DATE_ADDED,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.MIME_TYPE,
+                MediaStore.Audio.Media.SIZE,
+                MediaStore.Audio.Media.TITLE
         };
 
         Cursor cursor = getContentResolver().query(
@@ -74,13 +83,23 @@ public class MainActivity extends ActionBarActivity {
 
             // Create song object
             FireMixtape current = new FireMixtape(this);
+
             current._id = cursor.getString(0);
-            current.artist = cursor.getString(1);
-            current.title = cursor.getString(2);
-            current.data = cursor.getString(3);
-            current.display_name = cursor.getString(4);
-            current.duration = cursor.getString(5);
-            current.album_id = cursor.getString(6);
+            current.album = cursor.getString(1);
+            current.album_id = cursor.getString(2);
+            current.artist = cursor.getString(3);
+            current.artist_id = cursor.getString(4);
+            current.bookmark = cursor.getString(5);
+            current.composer = cursor.getString(6);
+            current.duration = cursor.getString(7);
+            current.title_key = cursor.getString(8);
+            current.year = cursor.getString(9);
+            current.data = cursor.getString(10);
+            current.date_added = cursor.getString(11);
+            current.display_name = cursor.getString(12);
+            current.mime_type = cursor.getString(13);
+            current.size = cursor.getString(14);
+            current.title = cursor.getString(15);
 
             data.add(current);
         }
