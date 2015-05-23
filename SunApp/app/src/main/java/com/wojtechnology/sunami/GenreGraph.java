97fd3f0849a4b228fdc8031f18797229a605d62f
wojtekswiderski <wswiders@uwaterloo.ca>
2015-05-23 14:43:25 -0400

    Converted all to AsyncTask.

--- SunApp/app/src/main/java/com/wojtechnology/sunami/GenreGraph.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/GenreGraph.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.wojtechnology.sunami.archiveJava.GenreBase;
@@ -39,100 +40,126 @@ public class GenreGraph {
     private Map<String, GenreVertex> mGenreRef;
     private Map<GenreVertex, List<GenreEdge>> mEdges;
 
+    private boolean isPopulated;
+
     public GenreGraph(Context context){
         this.mEdges = new HashMap<>();
         this.mGenreRef = new HashMap<>();
         this.context = context;
+        isPopulated = false;
         populateGraph();
     }
 
-    // Load data for genres.json resource
-    public void populateGraph() {
-        long startTime = Calendar.getInstance().getTimeInMillis();
-        InputStream is;
-        try {
-            is = context.openFileInput("genresStore.json");
-            Log.e("GenreGraph", "Open existing");
-        } catch (FileNotFoundException e) {
-            is = context.getResources().openRawResource(R.raw.genres);
-            Log.e("GenreGraph", "Open new");
-        }
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String line = reader.readLine();
-            String jString = "";
-            while (line != null) {
-                jString += line;
-                line = reader.readLine();
+    private class PopulateGraphTask extends AsyncTask<Void, Integer, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            long startTime = Calendar.getInstance().getTimeInMillis();
+            InputStream is;
+            try {
+                is = context.openFileInput("genresStore.json");
+                Log.e("GenreGraph", "Open existing");
+            } catch (FileNotFoundException e) {
+                is = context.getResources().openRawResource(R.raw.genres);
+                Log.e("GenreGraph", "Open new");
             }
-            reader.close();
-            is.close();
-            JSONArray ja = new JSONArray(jString);
-            for(int i = 0; i < ja.length(); i++){
-                JSONObject jo = ja.getJSONObject(i);
-                String genre = jo.getString("genre");
-                GenreVertex gv = new GenreVertex(genre, jo.getDouble("st"), jo.getDouble("lt"));
-                mGenreRef.put(genre, gv);
-            }
-            for(int i = 0; i < ja.length(); i++){
-                GenreVertex gv = mGenreRef.get(ja.getJSONObject(i).getString("genre"));
-                List<GenreEdge> edgeList = new ArrayList<>();
-                JSONArray subGenres = ja.getJSONObject(i).getJSONArray("assoc");
-                for(int j = 0; j < subGenres.length(); j++){
-                    GenreEdge subGenre = new GenreEdge(gv,
-                            mGenreRef.get(subGenres.getJSONObject(j).getString("name")),
-                            subGenres.getJSONObject(j).getDouble("similarity"));
-                    edgeList.add(subGenre);
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line = reader.readLine();
+                String jString = "";
+                while (line != null) {
+                    jString += line;
+                    line = reader.readLine();
                 }
-                mEdges.put(gv, edgeList);
+                reader.close();
+                is.close();
+                JSONArray ja = new JSONArray(jString);
+                for(int i = 0; i < ja.length(); i++){
+                    JSONObject jo = ja.getJSONObject(i);
+                    String genre = jo.getString("genre");
+                    GenreVertex gv = new GenreVertex(genre, jo.getDouble("st"), jo.getDouble("lt"));
+                    mGenreRef.put(genre, gv);
+                }
+                for(int i = 0; i < ja.length(); i++){
+                    GenreVertex gv = mGenreRef.get(ja.getJSONObject(i).getString("genre"));
+                    List<GenreEdge> edgeList = new ArrayList<>();
+                    JSONArray subGenres = ja.getJSONObject(i).getJSONArray("assoc");
+                    for(int j = 0; j < subGenres.length(); j++){
+                        GenreEdge subGenre = new GenreEdge(gv,
+                                mGenreRef.get(subGenres.getJSONObject(j).getString("name")),
+                                subGenres.getJSONObject(j).getDouble("similarity"));
+                        edgeList.add(subGenre);
+                    }
+                    mEdges.put(gv, edgeList);
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
+            Log.i("GenreGraph", "Finished populateGraph() in " +
+                    Long.toString(Calendar.getInstance().getTimeInMillis() - startTime) +
+                    " millis.");
+            return null;
         }
-        Log.i("GenreGraph", "Finished populateGraph() in " +
-                Long.toString(Calendar.getInstance().getTimeInMillis() - startTime) +
-                " millis.");
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            isPopulated = true;
+        }
+    }
+
+    public void populateGraph(){
+        new PopulateGraphTask().execute();
+    }
+
+    private class SaveGraphTask extends AsyncTask<Void, Integer, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            long startTime = Calendar.getInstance().getTimeInMillis();
+            try {
+                FileOutputStream fileOS = context.openFileOutput(
+                        "genresStore.json", Context.MODE_PRIVATE);
+                JSONArray ja = new JSONArray();
+                Set<GenreVertex> vertices = mEdges.keySet();
+                for (GenreVertex vertex : vertices){
+                    List<GenreEdge> edgeList = mEdges.get(vertex);
+                    JSONObject jo = new JSONObject();
+                    jo.put("genre", vertex.genre);
+                    jo.put("lt", vertex.longTerm);
+                    jo.put("st", vertex.shortTerm);
+                    JSONArray assocList = new JSONArray();
+                    for(GenreEdge edge : edgeList){
+                        JSONObject assocJO = new JSONObject();
+                        assocJO.put("name", edge.to.genre);
+                        assocJO.put("similarity", edge.similarity);
+                        assocList.put(assocJO);
+                    }
+                    jo.put("assoc", assocList);
+                    ja.put(jo);
+                }
+                fileOS.write(ja.toString().getBytes());
+                fileOS.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.i("GenreGraph", "Finished saveGraph() in " +
+                    Long.toString(Calendar.getInstance().getTimeInMillis() - startTime) +
+                    " millis.");
+            return null;
+        }
     }
 
     public void saveGraph(){
-        long startTime = Calendar.getInstance().getTimeInMillis();
-        try {
-            FileOutputStream fileOS = context.openFileOutput(
-                    "genresStore.json", Context.MODE_PRIVATE);
-            JSONArray ja = new JSONArray();
-            Set<GenreVertex> vertices = mEdges.keySet();
-            for (GenreVertex vertex : vertices){
-                List<GenreEdge> edgeList = mEdges.get(vertex);
-                JSONObject jo = new JSONObject();
-                jo.put("genre", vertex.genre);
-                jo.put("lt", vertex.longTerm);
-                jo.put("st", vertex.shortTerm);
-                JSONArray assocList = new JSONArray();
-                for(GenreEdge edge : edgeList){
-                    JSONObject assocJO = new JSONObject();
-                    assocJO.put("name", edge.to.genre);
-                    assocJO.put("similarity", edge.similarity);
-                    assocList.put(assocJO);
-                }
-                jo.put("assoc", assocList);
-                ja.put(jo);
-            }
-            fileOS.write(ja.toString().getBytes());
-            fileOS.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Log.i("GenreGraph", "Finished saveGraph() in " +
-                Long.toString(Calendar.getInstance().getTimeInMillis() - startTime) +
-                " millis.");
+        new SaveGraphTask().execute();
     }
 
     // Implement the update value method for a positive change in a genre
--- SunApp/app/src/main/java/com/wojtechnology/sunami/SongManager.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/SongManager.java
@@ -3,6 +3,7 @@ package com.wojtechnology.sunami;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.util.Log;
 
@@ -20,20 +21,25 @@ import java.util.Map;
 public class SongManager {
 
     private Context context;
-    private List<FireMixtape> fireMixtapes;
+    private List<FireMixtape> displayList;
+    private Map<String, FireMixtape> songDict;
+
+    private boolean genresUpdated;
 
     public SongManager(Context context) {
         this.context = context;
-        fireMixtapes = new ArrayList<>();
+        genresUpdated = false;
+        displayList = new ArrayList<>();
+        songDict = new HashMap<>();
         initSongs();
     }
 
     public void sortByTitle() {
-        if (fireMixtapes.size() <= 0){
+        if (displayList.size() <= 0){
             return;
         }
 
-        Collections.sort(fireMixtapes, new Comparator<FireMixtape>() {
+        Collections.sort(displayList, new Comparator<FireMixtape>() {
             @Override
             public int compare(FireMixtape lhs, FireMixtape rhs) {
                 String lTitle = lhs.title.toLowerCase();
@@ -57,15 +63,15 @@ public class SongManager {
         Map<Character, Integer> letters = new HashMap<>();
 
         int i = 0;
-        while(firstLetter(fireMixtapes.get(i).title) < 'A' ||
-                firstLetter(fireMixtapes.get(i).title) > 'Z'){
+        while(firstLetter(displayList.get(i).title) < 'A' ||
+                firstLetter(displayList.get(i).title) > 'Z'){
             i++;
         }
         int sum = i;
         int j = i;
 
-        for(char headerTitle = 'A'; headerTitle <= 'Z' && i < fireMixtapes.size(); i++){
-            if(firstLetter(fireMixtapes.get(i).title) != headerTitle){
+        for(char headerTitle = 'A'; headerTitle <= 'Z' && i < displayList.size(); i++){
+            if(firstLetter(displayList.get(i).title) != headerTitle){
                 letters.put(headerTitle, i - j);
                 headerTitle++;
                 j = i;
@@ -79,7 +85,7 @@ public class SongManager {
                 FireMixtape mixtape = new FireMixtape(context);
                 mixtape.title = Character.toString(headerTitle);
                 mixtape.genre = "__header__";
-                fireMixtapes.add(sum, mixtape);
+                displayList.add(sum, mixtape);
                 sum++;
             }
             sum += letters.get(headerTitle);
@@ -97,13 +103,13 @@ public class SongManager {
     }
 
     public List<FireMixtape> getFire() {
-        return fireMixtapes;
+        return displayList;
     }
 
     public FireMixtape getSong(String _id){
-        for(int i = 0; i < fireMixtapes.size(); i++){
-            if(fireMixtapes.get(i)._id == _id){
-                return fireMixtapes.get(i);
+        for(int i = 0; i < displayList.size(); i++){
+            if(displayList.get(i)._id == _id){
+                return displayList.get(i);
             }
         }
         return null;
@@ -156,7 +162,8 @@ public class SongManager {
             current.data = cursor.getString(7);
             current.size = cursor.getString(8);
 
-            fireMixtapes.add(current);
+            displayList.add(current);
+            songDict.put(current._id, current);
 
         }
 
@@ -165,33 +172,85 @@ public class SongManager {
         Log.i("SongManager", "Finished getFire() in " +
                 Long.toString(Calendar.getInstance().getTimeInMillis() - startTime) +
                 " millis.");
+
+        new UpdateGenresTask().execute();
     }
 
-    private void updateGenres(){
-        String[] genresProjection = {
-                MediaStore.Audio.Genres._ID,
-                MediaStore.Audio.Genres.NAME
-        };
+    private class UpdateGenresTask extends AsyncTask<Void, Integer, Void>{
 
-        Uri uri = MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI;
-        Cursor projectionCursor = context.getContentResolver().query(
-                uri,
-                genresProjection,
-                null,
-                null,
-                null
-        );
+        @Override
+        protected Void doInBackground(Void... params) {
+            long startTime = Calendar.getInstance().getTimeInMillis();
 
-        if (projectionCursor == null){
-            return;
+            //Some audio may be explicitly marked as not being music
+            String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+
+            String[] projection = {
+                    MediaStore.Audio.Media._ID,
+            };
+
+            String[] genresProjection = {
+                    MediaStore.Audio.Genres._ID,
+                    MediaStore.Audio.Genres.NAME
+            };
+
+            Uri uri = MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI;
+            Cursor projectionCursor = context.getContentResolver().query(
+                    uri,
+                    genresProjection,
+                    null,
+                    null,
+                    null
+            );
+
+            if (projectionCursor == null){
+                return null;
+            }
+
+            while (projectionCursor.moveToNext()) {
+
+                String genre_id = projectionCursor.getString(0);
+                String genre_name = projectionCursor.getString(1);
+
+                uri = MediaStore.Audio.Genres.Members.getContentUri("external", Long.parseLong(genre_id));
+
+                Cursor cursor = context.getContentResolver().query(
+                        uri,
+                        projection,
+                        selection,
+                        null,
+                        null
+                );
+
+                while (cursor.moveToNext()) {
+
+                    String _id = cursor.getString(0);
+                    songDict.get(_id).genre = genre_name;
+
+                }
+
+                cursor.close();
+
+            }
+            projectionCursor.close();
+
+            Log.i("SongManager", "Finished updateGenres() in " +
+                    Long.toString(Calendar.getInstance().getTimeInMillis() - startTime) +
+                    " millis.");
+
+            return null;
         }
 
-        while (projectionCursor.moveToNext()) {
-
-            String genre_id = projectionCursor.getString(0);
-            String genre_name = projectionCursor.getString(1);
-
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            genresUpdated = true;
         }
-        projectionCursor.close();
+    }
+
+    private void printSongs(){
+        for(int i = 0; i < displayList.size(); i++){
+            Log.i("SongManager", displayList.get(i).title + ": Genre - " + displayList.get(i).genre);
+        }
     }
 }
