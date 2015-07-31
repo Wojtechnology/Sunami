cf375e12508bb84c6a9158dfb6588c8925c4658c
wojtekswiderski <wswiders@uwaterloo.ca>
2015-07-30 23:03:12 -0400

    Added Concurrency to loading thumbnails.

--- SunApp/app/src/main/java/com/wojtechnology/sunami/ThumbnailManager.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/ThumbnailManager.java
@@ -1,6 +1,10 @@
 package com.wojtechnology.sunami;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.util.Pair;
 import android.widget.ImageView;
@@ -13,25 +17,33 @@ import java.lang.ref.WeakReference;
 public class ThumbnailManager {
 
     private MainActivity mContext;
+    private Bitmap mPlaceHolderBitmap;
 
     public ThumbnailManager(MainActivity context) {
         mContext = context;
+        mPlaceHolderBitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
     }
 
     public void setAlbumThumbnail(FireMixtape song, Pair<Integer, Integer> dimens, ImageView imageView) {
-        BitmapWorkerTask task = new BitmapWorkerTask(imageView, dimens);
-        task.execute(Long.parseLong(song.album_id));
+        Long album_id = Long.parseLong(song.album_id);
+        if (cancelPotentialWork(album_id, imageView)) {
+            BitmapWorkerTask task = new BitmapWorkerTask(imageView, dimens);
+            final AsyncDrawable asyncDrawable =
+                    new AsyncDrawable(mContext.getResources(), mPlaceHolderBitmap, task);
+            imageView.setImageDrawable(asyncDrawable);
+            task.execute(album_id);
+        }
     }
 
     class BitmapWorkerTask extends AsyncTask<Long, Void, Bitmap> {
 
         private final WeakReference<ImageView> imageViewReference;
         private Pair<Integer, Integer> paramDimens;
-        private long album_id;
+        private long album_id = 0;
 
         public BitmapWorkerTask(ImageView imageView, Pair<Integer, Integer> dimens) {
             paramDimens = dimens;
-            imageViewReference = new WeakReference<ImageView>(imageView);
+            imageViewReference = new WeakReference<>(imageView);
         }
 
         @Override
@@ -43,6 +55,10 @@ public class ThumbnailManager {
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
+            if (isCancelled()) {
+                bitmap = null;
+            }
+
             if (imageViewReference != null && bitmap != null) {
                 final ImageView imageView = imageViewReference.get();
                 if (imageView != null) {
@@ -50,5 +66,49 @@ public class ThumbnailManager {
                 }
             }
         }
+    }
+
+    static class AsyncDrawable extends BitmapDrawable {
+        private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+
+        public AsyncDrawable(Resources res, Bitmap bitmap,
+                             BitmapWorkerTask bitmapWorkerTask) {
+            super(res, bitmap);
+            bitmapWorkerTaskReference =
+                    new WeakReference<>(bitmapWorkerTask);
+        }
+
+        public BitmapWorkerTask getBitmapWorkerTask() {
+            return bitmapWorkerTaskReference.get();
+        }
+    }
+
+    public static boolean cancelPotentialWork(long album_id, ImageView imageView) {
+        final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+
+        if (bitmapWorkerTask != null) {
+            final long bitmapData = bitmapWorkerTask.album_id;
+            // If bitmapData is not yet set or it differs from the new data
+            if (bitmapData == 0 || bitmapData != album_id) {
+                // Cancel previous task
+                bitmapWorkerTask.cancel(true);
+            } else {
+                // The same work is already in progress
+                return false;
+            }
+        }
+        // No task associated with the ImageView, or an existing task was cancelled
+        return true;
+    }
+
+    private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
+        if (imageView != null) {
+            final Drawable drawable = imageView.getDrawable();
+            if (drawable instanceof AsyncDrawable) {
+                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+                return asyncDrawable.getBitmapWorkerTask();
+            }
+        }
+        return null;
     }
 }
