fcf1083fd92e95d897f9c213e20bef3bfbfd8bdc
wojtekswiderski <wswiders@uwaterloo.ca>
2015-08-20 19:46:16 -0400

    Added License

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ The android app that hand selects your music for you.
 Hopefully adding Spotify and SoundCloud integration depending on their cooperation.
 
 ```Made in Wojtechnology```
+
+This project is licensed under the GNU General Public License.
--- SunApp/app/src/main/java/com/wojtechnology/sunami/RemoteControlEventReceiver.java
+++ SunApp/app/src/main/java/com/wojtechnology/sunami/RemoteControlEventReceiver.java
@@ -1,3 +1,24 @@
+/*
+
+    Sunami - An Android music player which knows what you want to listen to.
+    Copyright (C) 2015 Wojtek Swiderski
+
+    Sunami is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Sunami is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    The GNU General Public License can be found at the root of this repository.
+
+    To contact me, email me at wojtek.technology@gmail.com
+
+ */
+
 package com.wojtechnology.sunami;
 
 import android.content.BroadcastReceiver;
